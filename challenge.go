829b31054c2d94070ba7e9e6bf30b12050f552d3
farhatnawaz <fariharal@gmail.com>
2017-04-10 00:14:34 +0500

    IP validation

--- challenge.go
+++ challenge.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
+//This is the actual function that helps access the remote machine and then execute the given command
 func runCmdRemotely(address string, command string) {
 
 	cmd := exec.Command("ssh", address, command) // exec.command would ask for the password anyway
@@ -22,15 +25,33 @@ func runCmdRemotely(address string, command string) {
 	fmt.Println(&out)
 }
 
+// This function validates the IP address entered by the user
+func isValidAddress(address string) bool {
+
+	var regex = "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+
+	s := strings.Split(address, "@")
+	match, _ := regexp.MatchString(regex, s[1])
+	return match
+}
+
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
+	var address string
+
 	// I know I had the choice to hard code the values instead of taking input from the user. I just thought
 	// it would seem more convenient to do it this way.
 
-	fmt.Println("Please enter the address (format user@address) : ")
-	address, _ := reader.ReadString('\n')
+	for {
+		fmt.Println("Please enter the address (format user@address) : ")
+		address, _ := reader.ReadString('\n')
+
+		if isValidAddress(address) {
+			break
+		}
+	}
 
 	fmt.Println("Please enter the command: ")
 	cmd, _ := reader.ReadString('\n')
